e0a5a7ff3d58f5bce102bd35a3aab7747123f895
Kevin Kweyu <kweyukevin@gmail.com>
2021-10-09 12:48:35 +0300

    Heroku push

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
-  get 'digital/inter'
-  get 'digital/redirector'
-  get 'students/class', to: 'students#class'
-  get 'students/school'
-  get 'students/margin'
-  get 'demo/index'
-  get 'demo/hello'
+  get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/create'
+  # get 'pages/edit'
+  # get 'pages/update'
+  # get 'pages/delete'
+  resources :pages
+
+  get 'subjects/index'
+  get 'subjects/', to: 'subjects#index'
+  # get 'subjects/:id', to: 'subjects#show'
+  # get 'subjects/new'
+  # get 'subjects/create'
+  # get 'subjects/edit'
+  # get 'subjects/update'
+  # get 'subjects/delete'
+  resources :subjects
+
+  # get 'digital/inter'
+  # get 'digital/redirector'
+
+  # get 'students/class', to: 'students#class'
+  # get 'students/school'
+  # get 'students/margin'
+
+  # get 'demo/index'
+  # get 'demo/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
